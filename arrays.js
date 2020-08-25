4a78aeebb554422da0777b2051d984a4b78bbc20
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-08-25 16:57:56 -0400

    added indexOf() example

---
+++ arrays.js
@@ -0,0 +1,10 @@
+// indexOf()
+favBands = ["Band 1", "Band 2", "Band 3", "Band 4"]
+var userBand = prompt("what is your fav band?")
+var a = favBands.indexOf(userBand)
+if (a > -1){
+    alert("Yeah, I love them")
+} else {
+    alert("Nah, they are lame")
+}
+
