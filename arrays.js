fd19ce1cf615a9bce39667d3a653db48b1a54ee4
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-08-28 16:52:37 -0400

    Added new function to remove array element by its value using splice

--- arrays.js
+++ arrays.js
@@ -8,3 +8,11 @@ if (a > -1){
     alert("Nah, they are lame")
 }
 
+// Removing Array Items By Value Using Splice
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+for( var i = 0; i < arr.length; i++){ 
+    if ( arr[i] === 5) { 
+        arr.splice(i, 1); 
+    }
+}
+//=> [1, 2, 3, 4, 6, 7, 8, 9, 0]
