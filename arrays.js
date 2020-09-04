8984d8b1adfc1718ff9b9a613f9b3ad3f06eb475
Hlsorrells <hlsorrells.dev@gmail.com>
2020-09-03 20:25:48 -0400

    added reverse array algorithm

--- arrays.js
+++ arrays.js
@@ -8,3 +8,16 @@ if (a > -1){
     alert("Nah, they are lame")
 }
 
+// A function that returns a new array with the data reversed without the use of .reverse() method
+var myArray = [1, 2, 3, 4, 5];
+
+const reverseArray = () => {
+    let revArray = [];
+    // Loop over the array in reverse order (start at the end and countdown)
+    for (var i = myArray.length - 1; i > -1; i--) {
+        revArray.push(myArray[i]);
+    }
+    console.log(revArray);
+}
+
+reverseArray(myArray);
