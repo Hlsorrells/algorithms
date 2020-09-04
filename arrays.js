964832fde2664cbc32b412697e8a4fbb93355d5e
Hlsorrells <hlsorrells.dev@gmail.com>
2020-09-03 20:35:41 -0400

    formatting changes

--- arrays.js
+++ arrays.js
@@ -1,22 +1,29 @@
 // indexOf()
-favBands = ["Band 1", "Band 2", "Band 3", "Band 4"]
-var userBand = prompt("what is your fav band?")
-var a = favBands.indexOf(userBand)
+favBands = ["Band 1", "Band 2", "Band 3", "Band 4"];
+var userBand = prompt("what is your fav band?");
+var a = favBands.indexOf(userBand);
+
 if (a > -1){
-    alert("Yeah, I love them")
+
+    alert("Yeah, I love them");
 } else {
-    alert("Nah, they are lame")
+
+    alert("Nah, they are lame");
 }
 
 // A function that returns a new array with the data reversed without the use of .reverse() method
 var myArray = [1, 2, 3, 4, 5];
 
 const reverseArray = () => {
+
     let revArray = [];
+
     // Loop over the array in reverse order (start at the end and countdown)
     for (var i = myArray.length - 1; i > -1; i--) {
+
         revArray.push(myArray[i]);
     }
+    
     console.log(revArray);
 }
 
