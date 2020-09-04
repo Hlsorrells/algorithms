7d924563b68c6c7d4902354f97b531978dbbd22b
Hlsorrells <hlsorrells.dev@gmail.com>
2020-09-03 21:01:44 -0400

    added countingOccurrences function to strings.js

--- strings.js
+++ strings.js
@@ -3,3 +3,10 @@
 const s = "This is the first string";
 const t = s.includes("first");
 console.log(t);
+
+// Write code to return the the number of vowels in `str`
+
+let vowels = ["a", "e", "i", "o", "u"]
+var vowelCount = str => str.toLowerCase().split("").filter(
+        ch => vowels.includes(ch)).length;
+console.log(vowelCount("i think, therefore i am."))
