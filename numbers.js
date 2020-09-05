b01e64d8b972ba1a71a8b3d47198e287fdb7d6e0
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-09-04 21:14:29 -0400

    add doSum function to sum integers from 1 to integer

--- numbers.js
+++ numbers.js
@@ -1,3 +1,17 @@
+// Write a function that takes in an integer and outputs the sum of all the numbers from 1 to that integer.
+
+function doSum(num) {
+    var result = 0;
+    for (let i = 0; i <= num; i++) {
+        result += i;
+    }
+    return result;
+}
+
+console.log(doSum(2));   // Output: 3 (because 1 + 2 = 3)
+console.log(doSum(4));   // Output: 10 (because 1 + 2 + 3 + 4 = 10)
+console.log(doSum(10));  // Output: 55
+
 // A function that returns M random unique non-negative integers less than some value N.
 // M = the number of random non-negative intergers that are less than the value of N
 // N = the upper limit of the value of each M random number
