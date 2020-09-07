fcbd60464fa62e91081eb610eb54939bc6bfbd92
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-09-07 19:15:32 -0400

    added fizz bizz algorithm to the Mathematics Folder

---
+++ Mathematics/fizzBizz.js
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////
+// Write code to loop through the array of numbers
+// At each iteration, if a number is evenly divisible by 3 print "Fizz"
+// If a number is evenly divisible by 5 print "Buzz"
+// If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
+// If a number is not divisible by 3 or 5, print the number
+var fizzBuzz = arr => {
+    for (var i = 0; i < arr.length; i++) {
+        if ((arr[i] % 3 === 0) && (arr[i] % 5 === 0)) {
+            console.log("Fizz Buzz")
+        }
+        else if (arr[i] % 3 === 0) {
+            console.log("Fizz")
+        }
+        else if (arr[i] % 5 === 0) {
+            console.log("Buzz")
+        }
+        else {
+            console.log(arr[i])
+        }
+    }
+};
