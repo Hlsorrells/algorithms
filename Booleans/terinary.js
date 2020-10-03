36603672585eeb218e71c9aa213a07d699e1c602
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-10-02 22:48:47 -0400

    add algorithm showing multi-level terinary

--- Booleans/terinary.js
+++ Booleans/terinary.js
@@ -1,3 +1,19 @@
 // Ternary
 return condition ? true : false
 // use of boolean operators like > or < will work
+
+
+// Ternary Hell
+// Write a function that returns the strings:
+
+// "both" if both given booleans a and b are true.
+// "first" if only a is true.
+// "second" if only b is true .
+// "neither" if both a and b are false.
+
+function areTrue(a, b) {
+    return a && b ? "both" 
+    : a ? "first" 
+    : b ? "second" 
+    : "neither"
+}
