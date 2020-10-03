e9d2e6602a6ee2b1956b072f0ae7c825831c8233
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-10-02 23:01:33 -0400

    add algorithm for example of multiple conditions in ternary

--- Booleans/ternary.js
+++ Booleans/ternary.js
@@ -2,6 +2,22 @@
 return condition ? true : false
 // use of boolean operators like > or < will work
 
+// Example of multiple conditions
+// Write a function that checks whether a person can watch an MA15+ rated movie. One of the following two conditions is required for admittance:
+
+// The person is at least 15 years old.
+// They have parental supervision.
+// The function accepts two parameters, age and isSupervised. Return a boolean.
+
+// Examples
+// acceptIntoMovie(14, true) ➞ true
+// acceptIntoMovie(14, false) ➞ false
+// acceptIntoMovie(16, false) ➞ true
+
+function acceptIntoMovie(age, isSupervised) {
+	return (age >= 15) || (isSupervised === true) ? true : false
+}
+
 
 // Ternary Hell
 // Write a function that returns the strings:
@@ -17,3 +33,4 @@ function areTrue(a, b) {
     : b ? "second" 
     : "neither"
 }
+
