289f2e6ece7c35879065653b46492a7d448d457f
Hlsorrells <hlsorrells.dev@gmail.com>
2021-09-12 17:00:19 -0400

    Add Math method file with info on the Math.min method.

---
+++ Methods/MathMethods.js
@@ -0,0 +1,5 @@
+// Math.min() expects each variable as separate param
+// ... spread operator
+    // allows an expression to be expanded in places where multiple elements/variables/arguments are expected
+
+const arrMin = arr => Math.min(...arr);
