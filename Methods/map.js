3bdff4d7992d9285f634a0a39012d0e94aff51f1
Hlsorrells <hlsorrells.dev@gmail.com>
2021-09-12 17:17:10 -0400

    Add algorithm using map to create an new array of objects

--- Methods/map.js
+++ Methods/map.js
@@ -1,3 +1,6 @@
+// Map() Method
+  // used to apply a function on every element in an array and returns a new array
+
 // Array.prototype.map()
 let newArr = oldArr.map((val, index, arr) => {
     // return element to new Array
@@ -21,3 +24,16 @@ let newArr = arr.map((v,i,a) => {
     }
   });
   // newArr = [1,4,3,8];
+
+  // Use map method to create an array of objects
+  let newObjArr = arr.map( (val, i, arr ) => {
+    return {
+      value: val,
+      index: i
+    }
+  })
+
+  // newObjArr = [
+  //   { value:1, index:0},
+  //   { value:2, index:1}
+  // ]
