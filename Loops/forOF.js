3993c29e1a4c25356a9d993fbad352b5ab11118a
Hlsorrells <hlsorrells.dev@gmail.com>
2021-09-12 17:28:34 -0400

    Add info on the for-of loop

---
+++ Loops/forOF.js
@@ -0,0 +1,9 @@
+// for...of Loop
+    // Creates a loop iterating over iterable objects like array, array-like objects (arguments or NodeList) Map, Set
+
+for (variable of iterable) {
+    statement
+}
+
+// variable - on each iteration a value of a different property is assigned to variable. Variable may be declared with const, let or var
+// iterable - object whose iterable properties are iterated
