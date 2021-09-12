58dc9740c9d267ec569bb9d1e6ee92eac9b91018
Hlsorrells <hlsorrells.dev@gmail.com>
2021-09-12 17:11:27 -0400

    Added info on the prototype filter method

--- Methods/filter.js
+++ Methods/filter.js
@@ -1,5 +1,12 @@
+// Filter() Method
+  // returns a new array created from all elements that pass a certain test performed on an original array
+
 // Array.prototype.filter()
 let newArr = oldArr.filter(callback);
+  // callback 
+    // test true - returns the element
+    // can take 3 arguments (element, index, arr)
+
 
 // filter()
 let even = arr.filter(val => {
@@ -10,3 +17,5 @@ let even = arr.filter(val => {
 // arrow function version
 let even = arr.filter(val => val % 2 === 0);
   // even = [2,4,6]
+
+  let newArray = oldArray
